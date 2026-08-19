1fa07e744832d4c9e91f712d1b2ae2270428b7d3
Mohammad Ziya <ziya7376502028@gmail.com>
2026-08-20 02:24:15 +0530

    get notified api in cs added

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import {  Routes, Route } from "react-router-dom";
+import {  Routes, Route, Navigate } from "react-router-dom";
 
 
 
@@ -43,8 +43,7 @@ export default function App() {
     
   </Route>
   <Route path="/document/:id" element={<SignDocument/>} />
-      {/* Redirect unknown routes
-      <Route path="*" element={<Navigate to="/" replace />} /> */}
+      <Route path="*" element={<Navigate to="/" replace />} /> 
     </Routes>
   );
 }
