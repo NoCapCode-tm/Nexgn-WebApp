4e96161713c67a5acbc779e20cca71238ebdf3b5
Mohammad Ziya <ziya7376502028@gmail.com>
2026-08-21 03:07:11 +0530

    login signup and invite

--- src/login-signup/pages/Login.jsx
+++ src/login-signup/pages/Login.jsx
@@ -1,17 +1,14 @@
 import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import LeftPanel from "../components/LeftPanel";
-import RightPanelCard from "../components/RightPanelCard";
-import useSystemTheme from "../hooks/useSystemTheme";
-import "../css/LoginSignup.css";
-import axios from "axios";
+import styles from "../css/LoginPage.module.css";
 import { API_URL } from "../../../config";
+import axios from "axios";
+import { useNavigate } from "react-router";
 
-export default function Login() {
-  useSystemTheme();
-  const [email, setEmail] = useState("");
+export default function LoginPage() {
+  const [email, setEmail] = useState("noar@nexgn.cloud");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate();
+
+   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -30,69 +27,84 @@ export default function Login() {
       console.log("Something went wrong", error.message);
     }
   };
-
   return (
-    <div className="login-signup-container">
-      <LeftPanel />
+    <div className={styles.page}>
 
-      <RightPanelCard
-        title="Welcome Back"
-        subtitle="Log in to continue to Nexgn"
-      >
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <div className="form-label-row">
-              <label htmlFor="email" className="form-label">
+      <div className={styles.leftPanel}>
+        <div className={styles.logo}>
+         
+          <svg width="51" height="51" viewBox="0 0 51 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M41 0C46.5228 0 51 4.47715 51 10V41C51 41.7841 50.906 42.5462 50.7354 43.2783L39.0234 31.8213C37.0495 29.8907 33.8666 29.908 31.9141 31.8604L30.5977 33.1777C28.6453 35.1302 28.6622 38.278 30.6357 40.209L41.6436 50.9766C41.4307 50.9901 41.2163 51 41 51H10C9.49485 51 8.99869 50.961 8.51367 50.8887L40.1865 19.9062C42.1604 17.9754 42.1777 14.8277 40.2256 12.875L38.9092 11.5576C36.9566 9.60506 33.7728 9.58757 31.7988 11.5186L0.113281 42.5127C0.0384312 42.0194 8.24792e-09 41.5142 0 41V10C0 9.76522 0.0104491 9.53249 0.0263672 9.30176L11.9219 20.9385C13.8959 22.8689 17.0788 22.8517 19.0312 20.8994L20.3477 19.583C22.3001 17.6306 22.283 14.4818 20.3096 12.5508L7.74316 0.257812C8.46859 0.0904411 9.22373 1.24512e-08 10 0H41Z" fill="#FF0915"/>
+</svg>
+        </div>
+
+        <div className={styles.formWrap}>
+          <h1 className={styles.title}>Welcome Back</h1>
+          <p className={styles.subtitle}>Log in to continue to Nexgn</p>
+
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <div className={styles.field}>
+              <label htmlFor="email" className={styles.label}>
                 Email
               </label>
+              <input
+                id="email"
+                type="email"
+                className={styles.input}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                autoComplete="email"
+              />
             </div>
-            <input
-              type="email"
-              id="email"
-              className="form-input"
-              placeholder="name@company.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
 
-          <div className="form-group">
-            <div className="form-label-row">
-              <label htmlFor="password" className="form-label">
-                Password
-              </label>
-              <a
-                href="#forgot"
-                className="forgot-password-link"
-                onClick={(e) => e.preventDefault()}
-              >
-                Forgot password?
+            <div className={styles.field}>
+              <div className={styles.labelRow}>
+                <label htmlFor="password" className={styles.label}>
+                  Password
+                </label>
+                <a href="#" className={styles.forgotLink}>
+                  Forgot password?
+                </a>
+              </div>
+              <input
+                id="password"
+                type="password"
+                className={styles.input}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                autoComplete="current-password"
+              />
+            </div>
+
+            <button type="submit" className={styles.loginButton}>
+              Login
+            </button>
+
+            <p className={styles.signupText}>
+              Don&apos;t have an account?{" "}
+              <a href="#" className={styles.signupLink}>
+                Sign up
               </a>
-            </div>
-            <input
-              type="password"
-              id="password"
-              className="form-input"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-
-          <button type="submit" className="form-submit-btn">
-            Login
-          </button>
-        </form>
-
-        <div className="form-card-footer">
-          Don't have an account?{" "}
-          <Link to="/signup" className="form-footer-link">
-            Sign up
-          </Link>
+            </p>
+          </form>
         </div>
-      </RightPanelCard>
+
+        <div className={styles.faceGraphic}>
+          <img src = "./Intersect.png" />
+        </div>
+
+        <p className={styles.tagline}>Smart Signing</p>
+
+        
+        <span className={styles.wordmarkRed} aria-hidden="true">
+          Nexgn
+        </span>
+      </div>
+
+      
+      <div className={styles.rightPanel}>
+        <span className={styles.wordmarkWhite}>Nexgn</span>
+      </div>
     </div>
   );
 }
